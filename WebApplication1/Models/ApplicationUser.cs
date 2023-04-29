9fa8bd85945f1e955c52de35376b574800d9cfa4
SaraBajraktari1 <100317308+SaraBajraktari1@users.noreply.github.com>
2023-04-29 16:16:10 -0700

    Add files via upload

---
+++ WebApplication1/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
+
+namespace WebApplication1.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        public string? StreetAddress { get; set; }   
+        public string? City { get; set; }
+        public int? Region { get; set; }
+        public string? PostalCode { get; set; }
+    }
+}
