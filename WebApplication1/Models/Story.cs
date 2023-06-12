8898e6a55c1995dedc6b6911b57eea5151b1c098
SaraBajraktari1 <100317308+SaraBajraktari1@users.noreply.github.com>
2023-06-12 16:48:49 -0700

    Add files via upload

---
+++ WebApplication1/Models/Story.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Story
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Car { get; set; }
+        public string Description { get; set; }
+    }
+}
