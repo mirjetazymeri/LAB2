22cd6b9245a47e368a5ba335c315f1c912667ee3
SaraBajraktari1 <100317308+SaraBajraktari1@users.noreply.github.com>
2023-06-12 16:50:06 -0700

    Add files via upload

---
+++ WebApplication1/Controllers/Stories1Controller.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Stories1Controller : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Stories1Controller(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Stories1
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Story>>> GetStories()
+        {
+          if (_context.Stories == null)
+          {
+              return NotFound();
+          }
+            return await _context.Stories.ToListAsync();
+        }
+
+        // GET: api/Stories1/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Story>> GetStory(int id)
+        {
+          if (_context.Stories == null)
+          {
+              return NotFound();
+          }
+            var story = await _context.Stories.FindAsync(id);
+
+            if (story == null)
+            {
+                return NotFound();
+            }
+
+            return story;
+        }
+
+        // PUT: api/Stories1/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStory(int id, Story story)
+        {
+            if (id != story.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(story).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Stories1
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Story>> PostStory(Story story)
+        {
+          if (_context.Stories == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Stories'  is null.");
+          }
+            _context.Stories.Add(story);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStory", new { id = story.Id }, story);
+        }
+
+        // DELETE: api/Stories1/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStory(int id)
+        {
+            if (_context.Stories == null)
+            {
+                return NotFound();
+            }
+            var story = await _context.Stories.FindAsync(id);
+            if (story == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stories.Remove(story);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StoryExists(int id)
+        {
+            return (_context.Stories?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/StoriesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Stories
+        public async Task<IActionResult> Index()
+        {
+              return _context.Stories != null ? 
+                          View(await _context.Stories.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Stories'  is null.");
+        }
+
+        // GET: Stories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Stories == null)
+            {
+                return NotFound();
+            }
+
+            var story = await _context.Stories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (story == null)
+            {
+                return NotFound();
+            }
+
+            return View(story);
+        }
+
+        // GET: Stories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Stories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Car,Description")] Story story)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(story);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(story);
+        }
+
+        // GET: Stories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Stories == null)
+            {
+                return NotFound();
+            }
+
+            var story = await _context.Stories.FindAsync(id);
+            if (story == null)
+            {
+                return NotFound();
+            }
+            return View(story);
+        }
+
+        // POST: Stories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Car,Description")] Story story)
+        {
+            if (id != story.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(story);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StoryExists(story.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(story);
+        }
+
+        // GET: Stories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Stories == null)
+            {
+                return NotFound();
+            }
+
+            var story = await _context.Stories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (story == null)
+            {
+                return NotFound();
+            }
+
+            return View(story);
+        }
+
+        // POST: Stories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Stories == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Stories'  is null.");
+            }
+            var story = await _context.Stories.FindAsync(id);
+            if (story != null)
+            {
+                _context.Stories.Remove(story);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StoryExists(int id)
+        {
+          return (_context.Stories?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
