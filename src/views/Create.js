04819315998d9489abc6f905e66f0df98343c800
HanaAnadolli <86631574+HanaAnadolli@users.noreply.github.com>
2023-06-14 16:25:46 +0200

    views added

---
+++ src/views/Create.js
@@ -0,0 +1,13 @@
+import { React } from 'react';
+import '../App.css';
+import CreateComponent from '../components/CreateComponent';
+
+function Create() {
+  return (
+    <div className="App">
+      <CreateComponent />
+    </div>
+  );
+}
+
+export default Create;
