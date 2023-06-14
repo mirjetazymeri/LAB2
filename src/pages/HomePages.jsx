2576fb4bc6e85f763a120d0f98b177bbed899d1a
HanaAnadolli <86631574+HanaAnadolli@users.noreply.github.com>
2023-06-14 16:24:18 +0200

    view pages added

---
+++ src/pages/HomePages.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Home from "../home/homes/Home"
+import Branding from "../home/Branding"
+
+const HomePages = () => {
+  return (
+    <>
+      <Home />
+      <Branding />
+    </>
+  )
+}
+
+export default HomePages
