f317cdbf0e9265e2df56f7309350a5866c892085
dionaymeraga <71883146+dionaymeraga@users.noreply.github.com>
2023-06-17 20:40:17 +0200

    Update CartPage.js

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 function CartPage({ cartItems, removeFromCart }) {
   const totalPrice = cartItems.reduce((total, item) => total + parseFloat(item.price), 0);
@@ -9,17 +10,34 @@ function CartPage({ cartItems, removeFromCart }) {
     removeFromCart(itemTitle);
   };
 
+  const checkout = async () => {
+    await fetch('http://localhost:4000/checkout', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({items: cartItems.items})
+    }).then((response) => {
+        return response.json();
+    }).then((response) => {
+        if(response.url) {
+            window.location.assign(response.url); // Forwarding user to Stripe
+        }
+    });
+}
+
   return (
     <div>
       <h1>Cart</h1>
       {cartItems.length === 0 ? (
-        <p>Your cart is empty.
-            <br />
-            <Link to='/shop'>Go to Shop</Link>
+        <p>
+          Your cart is empty.
+          <br />
+          <Link to="/shop">Go to Shop</Link>
         </p>
       ) : (
         <div>
-          <table style={{ width: "100%", border: "1px solid #ccc" }}>
+          <table style={{ width: '100%', border: '1px solid #ccc' }}>
             <thead>
               <tr>
                 <th>Product</th>
@@ -35,6 +53,9 @@ function CartPage({ cartItems, removeFromCart }) {
                   <td>
                     <button onClick={() => handleRemoveFromCart(item.title)}>Remove</button>
                   </td>
+                  <Button variant="success" onClick={checkout}>
+                                Purchase items!
+                            </Button>
                 </tr>
               ))}
               <tr>
